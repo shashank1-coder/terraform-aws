11bceb863534d3f49e071208db33ce94e2b7cf53
Shashank <bsathyashashank@gmail.com>
2025-04-04 11:43:41 +0530

    Changed instance region and type

--- Devops/Terraform_EC2_Creation.tf
+++ Devops/Terraform_EC2_Creation.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "ServerA" {
-  ami           = "ami-0c1ac8a41498c1a9c"
-  instance_type = "t3.micro"
+  ami           = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform-EC2"
